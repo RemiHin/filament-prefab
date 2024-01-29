081aa96a1b0d4a280d356aae481f2ac3e884b67c
Remi Hindriks <56312208+RemiHin@users.noreply.github.com>
2024-01-29 08:41:27 +0100

    updated icon

--- stubs/Modules/Base/App/Filament/Resources/PageResource.php
+++ stubs/Modules/Base/App/Filament/Resources/PageResource.php
@@ -19,7 +19,7 @@ class PageResource extends Resource
 {
     protected static ?string $model = Page::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-document-duplicate';
 
     public static function form(Form $form): Form
     {
