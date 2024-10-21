7ece2897018e9e53d189fe13c24cf19412075b07
daantimmerman <datimmerman@enrise.com>
2024-10-21 16:49:20 +0200

    Fix compliance for WCAG in tree component

--- stubs/Modules/Base/resources/views/components/menu/tree.blade.php
+++ stubs/Modules/Base/resources/views/components/menu/tree.blade.php
@@ -1,42 +1,34 @@
 <div class="md:flex justify-start items-center text-sm font-semibold text-brand-dark">
     @foreach($menu->children as $mainMenuItem)
-        <div class="relative group">
-            @if($mainMenuItem->children->count() || $mainMenuItem->menuable_type == 'Empty')
-                <button type="button"
-                        class="flex items-center gap-2 px-5 py-2.5">
-                    {{ $mainMenuItem->title }}
-                    @if($mainMenuItem->children->count())
-                        <x-svg src="assets/svg/chevron-down" class="hidden md:block h-5 w-5 text-gray-400"/>
-                    @endif
-                </button>
-            @endif
-
-            @if($mainMenuItem->menuable_type != 'Empty')
+        <ul class="list-none m-0 p-0">
+            @if($mainMenuItem->menuable_type == null)
                 <a href="{{ $mainMenuItem->getUrl() }}"
-                   class="{{ $mainMenuItem->children->count() ? 'hidden' : 'flex' }} items-center gap-2 px-5 py-2.5 bg-transparent text-gray-900 rounded-md mb-2 md:mb-0">
+                   class="{{ $mainMenuItem->children->count() ? 'hidden' : 'flex' }} items-center px-5 py-2.5 bg-transparent text-gray-900 rounded-md mb-2 md:mb-0">
+                    {{ $mainMenuItem->title }}
+                </a>
+            @elseif($mainMenuItem->children->count())
+                <li class="relative border-b border-slate-200 lg:border-none">
+                <span class="js-toggle-submenu flex lg:inline-flex items-center py-2 px-4 cursor-pointer underline underline-offset-4 decoration-2 decoration-transparent hover:decoration-current transition duration-150 ease-in-out text-brand-dark"
+                      tabindex="0"
+                      aria-expanded="false"
+                      role="button">
+                    {{ $mainMenuItem->title }}
+
+                    <x-svg class="svg-icon relative inline-flex self-center h-5 w-5 ml-2"
+                           src="assets/svg/chevron-down.svg"/>
+                </span>
+
+                    <x-menu.submenu :parent="$mainMenuItem"
+                                    :items="$mainMenuItem->children"
+                                    text-color="text-primary"
+                                    class="js-submenu relative flex-col gap-1.5 pl-4 mb-2 lg:gap-3 lg:absolute lg:min-w-full lg:right-0 lg:bottom-0 lg:transform lg:translate-y-full lg:bg-white lg:rounded lg:shadow-md lg:p-4 lg:mb-0"/>
+                </li>
+            @else
+                <a href="{{ $mainMenuItem->getUrl() }}"
+                   class="{{ $mainMenuItem->children->count() ? 'hidden' : 'flex' }} items-center px-5 py-2.5 bg-transparent text-gray-900 rounded-md mb-2 md:mb-0">
                     {{ $mainMenuItem->title }}
                 </a>
             @endif
-
-            @if($mainMenuItem->children->count())
-                <div
-                    class="absolute group-hover:block hidden md:left-0 pl-0 min-w-40 rounded-md md:shadow md:divide-y divide-gray-100 z-50"
-                >
-                    @if($mainMenuItem->menuable_type != 'Empty')
-
-                        <a href="{{ $mainMenuItem->getUrl() }}"
-                           class="hidden bg-white md:block w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm md:hover:bg-tint disabled:text-gray-500">
-                            {{ $mainMenuItem->title }}
-                        </a>
-                    @endif
-                    @foreach($mainMenuItem->children as $subMenuItem)
-                        <a href="{{ $subMenuItem->getUrl() }}"
-                           class="block w-full bg-white mb-2 md:mb-0 text-gray-900 rounded-md rounded-none first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-tint disabled:text-gray-500">
-                            {{ $subMenuItem->title }}
-                        </a>
-                    @endforeach
-                </div>
-            @endif
-        </div>
+        </ul>
     @endforeach
 </div>
