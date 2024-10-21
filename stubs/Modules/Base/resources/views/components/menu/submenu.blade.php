b1a08d0bea095da7b88f54a3d43ed9336c7b69f3
daantimmerman <datimmerman@enrise.com>
2024-10-21 17:07:44 +0200

    Fix compliance for WCAG in tree component

--- stubs/Modules/Base/resources/views/components/menu/submenu.blade.php
+++ stubs/Modules/Base/resources/views/components/menu/submenu.blade.php
@@ -9,24 +9,20 @@
 
     @if($parent)
         <li>
-            <a
-                href="{{ url($parent->path) }}"
-                class="flex py-1.5 underline underline-offset-4 decoration-2 decoration-transparent transition duration-150 ease-in-out {{ $textColor }} @if(url($parent->path) === url()->current()) font-bold @else hover:decoration-current @endif"
-                @if(url($parent->path) === url()->current()) aria-current="page" @endif
-            >
-                {{ $parent->name }}
+            <a href="{{ $parent->url }}"
+               class="flex py-1.5 underline underline-offset-4 decoration-2 decoration-transparent transition duration-150 ease-in-out {{ $textColor }} @if($parent->url === url()->current()) font-bold @else hover:decoration-current @endif"
+               @if($parent->url === url()->current()) aria-current="page" @endif>
+                {{ $parent->title }}
             </a>
         </li>
     @endif
 
     @forelse ($items as $item)
         <li>
-            <a
-                href="{{ url($item->path) }}"
-                class="flex py-1.5 underline underline-offset-4 decoration-2 decoration-transparent transition duration-150 ease-in-out {{ $textColor }} @if(url($item->path) === url()->current()) font-bold @else hover:decoration-current @endif"
-                @if(url($item->path) === url()->current()) aria-current="page" @endif
-            >
-                {{ $item->name }}
+            <a href="{{ $item->url }}"
+               class="flex py-1.5 underline underline-offset-4 decoration-2 decoration-transparent transition duration-150 ease-in-out {{ $textColor }} @if($item->url === url()->current()) font-bold @else hover:decoration-current @endif"
+               @if($item->url === url()->current()) aria-current="page" @endif>
+                {{ $item->title }}
             </a>
         </li>
     @empty
--- stubs/Modules/Base/resources/views/components/menu/tree.blade.php
+++ stubs/Modules/Base/resources/views/components/menu/tree.blade.php
@@ -6,12 +6,14 @@
                    class="{{ $mainMenuItem->children->count() ? 'hidden' : 'flex' }} items-center px-5 py-2.5 bg-transparent text-gray-900 rounded-md mb-2 md:mb-0">
                     {{ $mainMenuItem->title }}
                 </a>
-            @elseif($mainMenuItem->children->count())
+            @endif
+
+            @if($mainMenuItem->children->count())
                 <li class="relative border-b border-slate-200 lg:border-none">
-                <span class="js-toggle-submenu flex lg:inline-flex items-center py-2 px-4 cursor-pointer underline underline-offset-4 decoration-2 decoration-transparent hover:decoration-current transition duration-150 ease-in-out text-brand-dark"
-                      tabindex="0"
-                      aria-expanded="false"
-                      role="button">
+                    <span class="js-toggle-submenu flex lg:inline-flex items-center py-2 px-4 cursor-pointer underline underline-offset-4 decoration-2 decoration-transparent hover:decoration-current transition duration-150 ease-in-out text-brand-dark"
+                          tabindex="0"
+                          aria-expanded="false"
+                          role="button">
                     {{ $mainMenuItem->title }}
 
                     <x-svg class="svg-icon relative inline-flex self-center h-5 w-5 ml-2"
@@ -23,11 +25,6 @@
                                     text-color="text-primary"
                                     class="js-submenu relative flex-col gap-1.5 pl-4 mb-2 lg:gap-3 lg:absolute lg:min-w-full lg:right-0 lg:bottom-0 lg:transform lg:translate-y-full lg:bg-white lg:rounded lg:shadow-md lg:p-4 lg:mb-0"/>
                 </li>
-            @else
-                <a href="{{ $mainMenuItem->getUrl() }}"
-                   class="{{ $mainMenuItem->children->count() ? 'hidden' : 'flex' }} items-center px-5 py-2.5 bg-transparent text-gray-900 rounded-md mb-2 md:mb-0">
-                    {{ $mainMenuItem->title }}
-                </a>
             @endif
         </ul>
     @endforeach
