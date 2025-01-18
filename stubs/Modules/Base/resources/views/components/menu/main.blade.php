f4b0145a32b4d37e8c9efdaf0f618bef5047bcb9
Olindn <Omid177@gmail.com>
2025-01-18 08:59:07 +0100

    fix: typo in variable name $item -> $menuItem

--- stubs/Modules/Base/resources/views/components/menu/main.blade.php
+++ stubs/Modules/Base/resources/views/components/menu/main.blade.php
@@ -26,11 +26,11 @@
                 <a class="flex lg:inline-flex py-2 px-4 underline underline-offset-4 decoration-2 decoration-transparent transition duration-150 ease-in-out {{ $textColor }} @if($menuItem->getUrl() === url()->current()) font-bold @else hover:decoration-current @endif"
                    href="{{ $menuItem->getUrl() }}"
                    @if($menuItem->getUrl() === url()->current()) aria-current="page" @endif
-                   @if(is_external_url($item->getUrl())) target="_blank" rel="noopener" @endif
+                   @if(is_external_url($menuItem>getUrl())) target="_blank" rel="noopener" @endif
                 >
                     {{ $menuItem->title }}
 
-                    @if(is_external_url($item->getUrl()))
+                    @if(is_external_url($menuItem>getUrl()))
                         <x-svg
                             class="svg-icon relative inline-flex self-center h-6 w-6 ml-2"
                             src="assets/svg/external-link.svg"
